d19844ee8560e8ea60ea5fbe5ddda59d8ddfa3c2
Andy Gallagher <andy.gallagher@theguardian.com>
2018-07-24 10:31:52 +0100

    working etag calculator for both multi-chunk and single chunk uploads. working tests for ListParser

--- src/main/scala/EtagCalculator.scala
+++ src/main/scala/EtagCalculator.scala
@@ -1,8 +1,6 @@
 import java.io.File
 import java.io.FileInputStream
 import java.security.MessageDigest
-
-import org.apache.commons.codec.binary
 import org.apache.commons.codec.binary.Hex
 import org.slf4j.LoggerFactory
 
@@ -39,26 +37,33 @@ object EtagCalculator {
     val stream = new FileInputStream(file)
     logger.debug(s"calculating local etag for file ${file.getAbsolutePath}")
     logger.debug(s"file size is ${file.length} chunk size is ${chunkSize}")
-    val totalChunks = math.ceil(file.length()/chunkSize).toInt
+    val totalChunks = math.ceil(file.length() / chunkSize).toInt
     logger.debug(s"total chunks: $totalChunks")
-    val lastChunkSize=(file.length() - (totalChunks)*chunkSize).toInt
-    logger.debug(s"last chunk size: $lastChunkSize")
-    val md5Seq = Future.sequence(md5NextChunk(stream,0,chunkSize,lastChunkSize, totalChunks,Seq()))
+    if (totalChunks == 0) {
+      /*for an unchunked upload then the hash is simply the md5 of the entire file*/
+      val bytes = Array.fill[Byte](file.length().toInt)(0)
+      stream.read(bytes)
+      md5Of(bytes).map(bs=>Hex.encodeHexString(bs))
+    } else {
+      val lastChunkSize = (file.length() - totalChunks * chunkSize).toInt
+      logger.debug(s"last chunk size: $lastChunkSize")
+      val md5Seq = Future.sequence(md5NextChunk(stream, 0, chunkSize, lastChunkSize, totalChunks, Seq()))
 
-    /* ensure that the FileInputStream is closed */
-    md5Seq.onComplete({
-      case Failure(err)=>
-        logger.error(s"${file.getAbsolutePath}: Could not calculate md5 chain", err)
-        stream.close()
-      case Success(seq)=>
-        logger.debug(s"${file.getAbsolutePath}: Calculated md5 chain")
-        stream.close()
-    })
+      /* ensure that the FileInputStream is closed */
+      md5Seq.onComplete({
+        case Failure(err) =>
+          logger.error(s"${file.getAbsolutePath}: Could not calculate md5 chain", err)
+          stream.close()
+        case Success(seq) =>
+          logger.debug(s"${file.getAbsolutePath}: Calculated md5 chain")
+          stream.close()
+      })
 
-    /* reduce the list of portions back to a single one */
-    md5Seq.flatMap(md5Parts=>{
-      val finalByteString:Array[Byte] = md5Parts.reduce((acc,item)=>acc++item)
-      md5Of(finalByteString).map(bs=>Hex.encodeHexString(bs) + s"-${totalChunks+1}")
-    })
+      /* reduce the list of portions back to a single one */
+      md5Seq.flatMap(md5Parts => {
+        val finalByteString: Array[Byte] = md5Parts.reduce((acc, item) => acc ++ item)
+        md5Of(finalByteString).map(bs => Hex.encodeHexString(bs) + s"-${totalChunks + 1}")
+      })
+    }
   }
 }
--- src/test/scala/ListParserSpec.scala
+++ src/test/scala/ListParserSpec.scala
@@ -1,5 +1,28 @@
+import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+import org.specs2.mock.Mockito
 
-class ListParserSpec extends Specification{
+@RunWith(classOf[JUnitRunner])
+class ListParserSpec extends Specification with Mockito {
+  "ListParser" should {
+    "call the passed block once for each project, filepath pair" in {
+      val lp = new ListParser("src/test/resources/sample1.lst")
+      val mockFunction = mock[Function2[String,String,Unit]]
 
+      lp.foreach(mockFunction)
+
+      there was one(mockFunction).apply("1234","/path/to/firstasset.mxf") andThen one(mockFunction).apply("1234","/path/to/secondasset.mxf") andThen one(mockFunction).apply("4567","/path/to/another/setofassets/thing.mov")
+    }
+
+    "skip over invalid lines" in {
+      val lp = new ListParser("src/test/resources/sample2.lst")
+      val mockFunction = mock[Function2[String,String,Unit]]
+
+      lp.foreach(mockFunction)
+
+      there was one(mockFunction).apply("1234","/path/to/firstasset.mxf") andThen one(mockFunction).apply("1234","/path/to/secondasset.mxf") andThen one(mockFunction).apply("4567","/path/to/another/setofassets/thing.mov")
+
+    }
+  }
 }
